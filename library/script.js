e6840dbd33c326a5bf1e02b6e7b21f3958546b69
Carlos Escobar <carlosescobar@weblabor.mx>
2020-09-24 13:39:31 -0500

    Fix url

--- library/script.js
+++ library/script.js
@@ -1,5 +1,5 @@
-//var url = "https://weblabormx.github.io/Easy-JS-Library/library/";
-var url = "http://easy-js-library.test/library/";
+var url = "https://weblabormx.github.io/Easy-JS-Library/library/";
+//var url = "http://easy-js-library.test/library/";
 
 jQuery(document).ready(function($){
     $.getScript(url+'script-old.js', function(){
