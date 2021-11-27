3a35a6db58ab4483cea7bf7c853f6647f8627ef0
Carlos Escobar <carlos@xmlteam.com>
2021-11-27 11:58:05 -0600

    Move carousel from old to new, update documentation

--- library/script-new.js
+++ library/script-new.js
@@ -254,7 +254,7 @@ function EasyController() {
 function EasyJsLibrary() {
     
     this.url = "https://weblabormx.github.io/Easy-JS-Library/library/";
-    //this.url = "http://easy-js-library.oo/library/";
+    //this.url = "http://easy-js-library.test/library/";
     this.controller = new EasyController();
 
     this.execute = function() { 
