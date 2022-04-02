9f06a4bc9bde29056bce797ca2c73bbcfc90d22b
Luan Himmlisch <luisangelserranocatala@gmail.com>
2022-04-01 23:56:22 -0600

    Ready for PR

--- library/script.js
+++ library/script.js
@@ -1,5 +1,5 @@
-// var url = "https://weblabormx.github.io/Easy-JS-Library/library/";
-var url = "http://localhost:8080/library/";
+var url = "https://weblabormx.github.io/Easy-JS-Library/library/";
+// var url = "http://localhost:8080/library/";
 
 jQuery(document).ready(function ($) {
     $.getScript(url + 'script-old.js', function () {
